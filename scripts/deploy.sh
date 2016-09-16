0236d9d906936ba2c8d670de47cbb1085ecd3114
Mathieu Desvé <mathieu@ogury.co>
2016-09-16 14:21:35 +0200

    Initial Commit

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Deploy $TRAVIS_TAG version to S3"
+aws s3 cp infra/resources.cform s3://chatanoo-deployment/infra/resources/$TRAVIS_TAG.cform
+
+echo "Upload latest"
+aws s3api put-object --bucket chatanoo-deployment --key infra/resources/latest.cform --website-redirect-location /infra/resources/$TRAVIS_TAG.cform
