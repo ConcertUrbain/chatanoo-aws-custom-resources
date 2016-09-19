64b40c4310ed88903473a8f0e7fea5442567ffba
Mathieu Desvé <mathieu@ogury.co>
2016-09-19 14:42:22 +0200

    Fix cloudformation script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,7 @@ echo "Deploy $TRAVIS_TAG version to S3"
 aws s3 cp infra/resources.cform s3://chatanoo-deployment/infra/resources/$TRAVIS_TAG.cform
 
 echo "Upload latest"
-aws s3api put-object --bucket chatanoo-deployment --key infra/resources/latest.cform --website-redirect-location /infra/resources/$TRAVIS_TAG.cform
+aws s3api put-object \
+  --bucket chatanoo-deployment \
+  --key infra/resources/latest.cform \
+  --website-redirect-location /infra/resources/$TRAVIS_TAG.cform
