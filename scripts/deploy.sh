19ae7b2ea69a2e70e1befc90ac19aa90c17bd995
Mathieu Desvé <mathieu@ogury.co>
2016-11-14 11:23:25 +0100

    Configure Travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Deploy $TRAVIS_TAG version to S3"
-aws s3 cp infra/resources.cfn.yml s3://chatanoo-deployment.eu-west-1/infra/resources/$TRAVIS_TAG.cfn.yml
+aws s3 cp infra/resources.cfn.yml s3://chatanoo-deployments-eu-west-1/infra/resources/$TRAVIS_TAG.cfn.yml
 
 echo "Upload latest"
 aws s3api put-object \
-  --bucket chatanoo-deployment.eu-west-1 \
+  --bucket chatanoo-deployments-eu-west-1 \
   --key infra/resources/latest.cfn.yml \
   --website-redirect-location /infra/resources/$TRAVIS_TAG.cfn.yml
